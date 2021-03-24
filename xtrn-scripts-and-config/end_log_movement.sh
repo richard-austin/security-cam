9e6589c2b0693355b6d89345f70627e82f49f67e
Richard Austin <richard.david.austin@gmail.com>
2021-03-24 20:11:07 +0000

    log movement scripts changed to check for success of start and stop recording and retry if it fails. This is needed because the start and stop recording function is particularly unreliable on the Raspberry pi

--- xtrn-scripts-and-config/end_log_movement.sh
+++ xtrn-scripts-and-config/end_log_movement.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 log_file=/home/www-data/motion-log/motionevents-`date +%Y%m%d`.log
-
 IFS=':' read -ra FIELDS <<< $2
-echo "`date`: curl \"http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
-curl "http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}" >> $log_file
-echo -e " " >> $log_file 
+
+stop ()
+{
+	echo "`date +%d-%M-%Y" "%T`: curl \"http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
+	result=`curl "http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}"`
+	echo -e $result >> $log_file
+}
+
+
+while :
+do
+	stop
+	[[ $result == "" ]] || break
+	sleep 1
+done
--- xtrn-scripts-and-config/log_movement.sh
+++ xtrn-scripts-and-config/log_movement.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 log_file=/home/www-data/motion-log/motionevents-`date +%Y%m%d`.log
-
 IFS=':' read -ra FIELDS <<< $2
-echo "`date +%d-%M-%Y" "%T`: curl \"http://localhost:8083/control/record/start?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
-curl "http://localhost:8083/control/record/start?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}" >> $log_file
-echo -e " " >> $log_file
+
+start () {
+	echo "`date +%d-%M-%Y" "%T`: curl \"http://localhost:8083/control/record/start?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
+	result=`curl "http://localhost:8083/control/record/start?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}"`
+	echo -e $result >> $log_file
+}
+
+stop ()
+{
+	echo "`date +%d-%M-%Y" "%T`: curl \"http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
+	result=`curl "http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}"`
+}
+
+
+while :
+do
+	start
+	[[ $result == "" ]] || break
+	sleep 1
+	stop
+	sleep 1
+done
