c194edc65cc38c38c28aef35290d9182a16c3081
Richard Austin <richard.david.austin@gmail.com>
2021-03-24 14:01:52 +0000

    Config file changes.

--- xtrn-scripts-and-config/end_log_movement.sh
+++ xtrn-scripts-and-config/end_log_movement.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-IFS=':' read -ra FIELDS <<< $2
-curl "http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}"
+log_file=/home/www-data/motion-log/motionevents-`date +%Y%m%d`.log
 
-echo "curl \"http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> /home/www-data/motionevents.log
+IFS=':' read -ra FIELDS <<< $2
+echo "`date`: curl \"http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}\"" >> $log_file
+curl "http://localhost:8083/control/record/stop?app=${FIELDS[1]}&name=${FIELDS[2]}&rec=${FIELDS[3]}" >> $log_file
+echo -e " " >> $log_file 
