3bb85e77baeccf23dc22d06a1e7cf55247350eb5
Richard Austin <richard.david.austin@gmail.com>
2021-04-09 19:33:23 +0100

    API and service calls to get version.

--- server/grails-app/controllers/server/UtilsController.groovy
+++ server/grails-app/controllers/server/UtilsController.groovy
@@ -10,17 +10,28 @@ import security.cam.interfaceobjects.ObjectCommandResponse
 class UtilsController {
     UtilsService utilsService
     /**
-    *getTemperature: Get the core temperature (Raspberry pi only). This is called at intervals to keep the session alive
-    * @return: The temperature as a string. On non Raspberry pi systems an error is returned.
-    */
+     * getTemperature: Get the core temperature (Raspberry pi only). This is called at intervals to keep the session alive
+     * @return: The temperature as a string. On non Raspberry pi systems an error is returned.
+     */
     @Secured(['ROLE_CLIENT'])
     def getTemperature() {
         ObjectCommandResponse response = utilsService.getTemperature()
 
-        if(response.status != PassFail.PASS)
-            render new Temperature("Temp=47.98'C") as JSON
+        if (response.status != PassFail.PASS)
+            render(status: 500, text: response.error)
+        else
+            render response.responseObject as JSON
+    }
 
-        //   render(status: 500, text: response.error)
+    /**
+     * getVersion: Get the version number from application.yml config
+     * @return: The version strig
+     */
+    @Secured(['ROLE_CLIENT'])
+    def getVersion() {
+        ObjectCommandResponse response = utilsService.getVersion()
+        if (response.status != PassFail.PASS)
+            render(status: 500, text: response.error)
         else
             render response.responseObject as JSON
     }
--- server/setVersion.sh
+++ server/setVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -x
+# set -x
 DATE=`date +%d-%m-%Y-%T`
-sed -i -e "/version\:/s/\:.*/: '`git describe --tags` `echo $DATE`'/" grails-app/conf/application.yml
+sed -i -e "/securityCamVersion\:/s/\:.*/: '`git describe --tags --dirty`'/" grails-app/conf/application.yml
