12019f26103c527c5ad38f6cd9064ecfa1202b16
Richard Austin <richard.david.austin@gmail.com>
2021-04-14 21:18:28 +0100

    Update the bash script which checks for IP change to not update the myip file so this can be done with a web app call instead. Also update the email text to reflect this.

--- xtrn-scripts-and-config/check_ip_not_changed.sh
+++ xtrn-scripts-and-config/check_ip_not_changed.sh
@@ -13,21 +13,26 @@ Subject: Change of public IP address
 
 Hi Richard,
 
-I have detected a change of Virgin Media broadband IP address, this is now ${current_ip}	
+I have detected a change of Virgin Media broadband IP address, this is now ${current_ip}.
+
+Please go to the web application at the new address and use the Set IP option to stop these emails continuing to be sent.
 
 Thanks
 
 Raspberry pi
 EOT
 
+# Send the email
 curl --ssl-reqd \
   --url 'smtps://smtp.virginmedia.com:465' \
   --user 'rdaustin@virginmedia.com:DC10plus' \
   --mail-from 'rdaustin@virginmedia.com' \
   --mail-rcpt 'richard.david.austin@gmail.com' \
   --upload-file /home/www-data/changed_ip_email.txt
-
-
 fi
 
-echo $current_ip > /home/www-data/myip
+# The myip file will now be updated with an API call. That way notifications will continue to
+#  be sent until the user responds on the new IP address. As IP changes are mst likely to be accompanied by
+#  some sort of outage, this method is more likely to get an email through than sending just one close to an outage
+#  then resetting straight away.
+# echo $current_ip > /home/www-data/myip
