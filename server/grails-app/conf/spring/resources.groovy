8fc14361f040f4a5a3822d073f06718b53372a26
Richard Austin <richard.david.austin@gmail.com>
2021-04-16 15:20:28 +0100

    Auditing of login, logout and login failure events.

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,7 +1,19 @@
 import security.cam.commands.UserPasswordEncoderListener
+import security.cam.eventlisteners.SecCamAuthFailEventListener
+import security.cam.eventlisteners.SecCamSecurityEventListener
 
 
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+
+    // This bean audits user logins and logouts
+    secCamSecurityEventListener(SecCamSecurityEventListener) {
+        logService = ref("logService")
+    }
+
+    // This bean audits failed user logins
+    secCamAuthFailEventListener(SecCamAuthFailEventListener) {
+        logService = ref("logService")
+    }
 }
