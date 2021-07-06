e38cafc0f49cbe5383ef15fd6d99045e7ec09600
richard-austin <richard.david.austin@gmail.com>
2021-07-06 16:47:32 +0100

    Incorporate check_ip_not_changed into sc_processes.sh. Check ip every 15 minutes, if it has changed send email and wait for 1 hour before sending again. When ip is set to the new one, revert to the 15 minute loop.

--- xtrn-scripts-and-config/end_hd_recording.sh
+++ xtrn-scripts-and-config/end_hd_recording.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 log_file=/home/security-cam/motion-log/motionevents-$(date +%Y%m%d).log
-IFS=':' read -ra FIELDS <<< "$2"
+IFS=':' read -ra FIELDS <<<"$2"
 
-stop
-  PID=$(< /home/security-cam/recording-pids/"${FIELDS[1]}-${FIELDS[2]}".pid)
+stop() {
+  PID=$(</home/security-cam/recording-pids/"${FIELDS[1]}-${FIELDS[2]}".pid)
   kill -INT "$PID"
-  echo "$(date +%d-%m-%Y" "%T): Stopped recording, pid $PID" >> "$log_file"
+  echo "$(date +%d-%m-%Y" "%T): Stopped recording, pid $PID" >>"$log_file"
+}
 
 stop
-
-/etc/security-cam/check_ip_not_changed.sh
