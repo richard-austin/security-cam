e28e8972e96a620f4c3127d59bfe9baf4ec19a28
richard-austin <richard.david.austin@gmail.com>
2021-07-21 13:23:25 +0100

    Increase recording retention time to three weeks.

--- xtrn-scripts-and-config/start_hd_recording.sh
+++ xtrn-scripts-and-config/start_hd_recording.sh
@@ -9,7 +9,7 @@ start () {
 	echo "$(date +%d-%m-%Y" "%T): Started recording: PID $(cat /home/security-cam/recording-pids/"${FIELDS[1]}-${FIELDS[2]}".pid): - ffmpeg -i rtmp://localhost:1935/${FIELDS[1]}/${FIELDS[2]}" -an -c copy -level 3.0 -start_number 0 -hls_time 3 -hls_list_size 0 -f hls /home/security-cam/"${FIELDS[3]}/${FIELDS[1]}-$(date "+%s")_.m3u8" >> "${log_file}"
 
   # Remove oldest recording files (older than 2 weeks)
-  find /home/security-cam/"${FIELDS[3]}" -mmin +20160 -exec rm {} \;
+  find /home/security-cam/"${FIELDS[3]}" -mmin +30240 -exec rm {} \;
 }
 
 start
