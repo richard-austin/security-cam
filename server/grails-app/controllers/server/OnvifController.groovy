bbf24e9d65752f62e47d226fe76bdb4d084d5fad
austinr <richard.david.austin@gmail.com>
2021-11-06 19:27:51 +0000

    Set up some accessors for getting camera info.
    Look into runtime warnings relating to dependencies.

--- server/grails-app/controllers/server/OnvifController.groovy
+++ server/grails-app/controllers/server/OnvifController.groovy
@@ -3,7 +3,12 @@ package server
 import grails.plugin.springsecurity.annotation.Secured
 import onvif.discovery.OnvifDiscovery
 import onvif.soap.OnvifDevice
+import onvif.utils.OnvifUtils
 import org.apache.commons.io.FileUtils
+import org.onvif.ver10.media.wsdl.Media
+import org.onvif.ver10.schema.AudioEncoderConfiguration
+import org.onvif.ver10.schema.AudioOutputConfiguration
+import org.onvif.ver10.schema.Profile
 import org.utils.OnvifCredentials
 import org.utils.TestDevice
 import security.cam.LogService
@@ -11,6 +16,35 @@ import security.cam.LogService
 class OnvifController {
     LogService logService
 
+    static String getDetail(String details, String field)
+        throws Exception
+    {
+        int startIndex = details.indexOf(field), endIndex
+        if(startIndex == -1)
+            throw new Exception("No such field ${field}")
+        startIndex = details.indexOf('[', startIndex)
+        if(startIndex == -1)
+            throw new Exception('No opening [ found')
+        endIndex = startIndex
+
+        String restOfDetails = details.substring(startIndex)
+        // Find corresponding closing ]
+
+        int bracketCount = 1, idx = 1
+        while (bracketCount > 0 && endIndex < details.length())
+        {
+            String nextChar = restOfDetails[idx]
+            if(nextChar == '[')
+                ++bracketCount
+            else if(nextChar == ']')
+                --bracketCount
+
+            endIndex = startIndex + ++idx
+        }
+        if(endIndex >= details.length())
+            throw new Exception('Could not find matching closing ]')
+        return details.substring(startIndex, endIndex)
+    }
 
     @Secured(['ROLE_CLIENT'])
     def discover() {
@@ -35,6 +69,27 @@ class OnvifController {
 
                     System.out.println("Connect to camera, please wait ...")
                     cam = new OnvifDevice(credentials.getHost(), credentials.getUser(), credentials.getPassword())
+
+                    Media media = cam.getMedia()
+                    media.getVideoSources()
+                    List<Profile> profiles = media.getProfiles()
+
+                    profiles.forEach({Profile profile ->
+                        String profileToken = profile.getToken();
+                        String name = profile.getName()
+                        String rtsp = cam.getStreamUri(profileToken)
+                        AudioOutputConfiguration audio1
+
+                        List<AudioEncoderConfiguration> audio2 =  media.getCompatibleAudioEncoderConfigurations(profileToken)
+ //                       audio1 =  media.getAudioOutputConfiguration(audio2[0].getToken())
+                        AudioEncoderConfiguration aconfig = media.getAudioEncoderConfiguration(audio2[0].getToken())
+                        def info = cam.getDeviceInfo()
+                        def details = OnvifUtils.format(profile)
+                        def x = getDetail(details, 'videoEncoderConfiguration')
+                        def y = getDetail(x, 'resolution')
+                        def z = y
+                    })
+
                     System.out.printf("Connected to device %s (%s)%n", cam.getDeviceInfo(), cam.streamUri.toString())
                     System.out.println(TestDevice.inspect(cam))
 
