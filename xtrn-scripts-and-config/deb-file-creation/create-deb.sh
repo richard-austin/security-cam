6b391cd0b11c08fbd75551adf085d6b949f22850
austinr <richard.david.austin@gmail.com>
2022-12-04 11:32:52 +0000

    Revert to previous version of motion (from apt install motion) instead of the built version which supports setting udp (not using it now). There were a lot of missed motion events since the newer version was running.

--- xtrn-scripts-and-config/deb-file-creation/create-deb.sh
+++ xtrn-scripts-and-config/deb-file-creation/create-deb.sh
@@ -20,15 +20,9 @@ mkdir -p security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-serv
 cp  ../../camera-recordings-service/src/*.py security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
 cp  ../../camera-recordings-service/src/requirements.txt security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
 
-mkdir -p security-cam_"${VERSION}"_arm64/usr/bin
-mkdir -p security-cam_"${VERSION}"_arm64/etc/motion
-
 mkdir -p security-cam_"${VERSION}"_arm64/lib/systemd/system
 cp ../wifimanagement.service  security-cam_"${VERSION}"_arm64/lib/systemd/system
 cp ../camera-recordings.service security-cam_"${VERSION}"_arm64/lib/systemd/system
-cp ../motion/motion.service security-cam_"${VERSION}"_arm64/lib/systemd/system/motion.service
-cp ../motion/motion security-cam_"${VERSION}"_arm64/usr/bin
-cp ../motion/motion.conf security-cam_"${VERSION}"_arm64/etc/motion
 
 tar -xf nms.tar --directory security-cam_"${VERSION}"_arm64/etc/security-cam
 
@@ -57,7 +51,9 @@ mkdir -p security-cam_"${VERSION}"_arm64/var/lib/tomcat
 
 mkdir -p security-cam_"${VERSION}"_arm64/tmp
 
-cp -r ../nginx.conf ../chrony.conf ../ssmtp.conf security-cam_"${VERSION}"_arm64/tmp
+mkdir -p security-cam_"${VERSION}"_arm64/lib/systemd/system/
+
+cp -r ../motion/motion.conf ../nginx.conf ../chrony.conf ../ssmtp.conf security-cam_"${VERSION}"_arm64/tmp
 cp ../apache-tomcat-9.0.46/conf/server.xml ../apache-tomcat-9.0.46/conf/tomcat-users.xml security-cam_"${VERSION}"_arm64/tmp
 cp ../install-cert.sh ../tomcat9 security-cam_"${VERSION}"_arm64/tmp
 cp ../../server/build/libs/server-7.3.war security-cam_"${VERSION}"_arm64/tmp
@@ -70,6 +66,7 @@ Maintainer: Richard Austin <richard.david.austin@gmail.com>
 Description: A security camera system accessed through a secure web based interface.
 Depends: openjdk-17-jre-headless (>=17.0.3), openjdk-17-jre-headless (<< 18.0.0),
  ffmpeg (>=7:4.2.4), ffmpeg (<<7:5.0.0),
+ motion (>=4.3.2-1), motion(<<5.0.0-0),
  nginx (>=1.18.0), nginx(<=1.20.9),
  tomcat9 (>=9.0.43-1), tomcat9 (<= 10.0.0),
  tomcat9-admin (>=9.0.43-1), tomcat9-admin (<= 10.0.0),
@@ -78,8 +75,7 @@ Depends: openjdk-17-jre-headless (>=17.0.3), openjdk-17-jre-headless (<< 18.0.0)
  network-manager (>= 1.36.4), network-manager (<< 2.0.0),
  wireless-tools (>=30~pre9-13), wireless-tools (<< 40),
  moreutils,
- python3-pip, autoconf, automake, libtool, libavcodec-dev, libavdevice-dev, libavformat-dev, libswscale-dev, libjpeg-dev,
- libpq-dev, libsqlite3-dev, debhelper, dh-autoreconf, libwebp-dev, libmicrohttpd-dev, gettext, libmariadb-dev
+ python3-pip
 EOF
 
 dpkg-deb --build --root-owner-group security-cam_"${VERSION}"_arm64
