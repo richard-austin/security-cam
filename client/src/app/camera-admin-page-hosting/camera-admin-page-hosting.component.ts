d9386fe6433a96d49274d17d63d3b405759ffea7
richard-austin <richard.david.austin@gm>
2023-03-30 18:43:56 +0100

    Fix bug where switching between camera admin pages left the resetTimer calls running.

--- client/src/app/camera-admin-page-hosting/camera-admin-page-hosting.component.ts
+++ client/src/app/camera-admin-page-hosting/camera-admin-page-hosting.component.ts
@@ -15,42 +15,47 @@ import {environment} from '../../environments/environment';
 export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, OnDestroy {
   cam!: Camera;
   accessToken!: string;
-  intervalSubscription!: Subscription;
+  intervalSubscription: Subscription | undefined;
   hostServiceUrl!: SafeResourceUrl;
   @ViewChild(ReportingComponent) reporting!: ReportingComponent;
-  private readonly defaultPort: number =80; // TODO: Probably need to set the port in the camera info as it may not always be 80
+  private readonly defaultPort: number = 80; // TODO: Probably need to set the port in the camera info as it may not always be 80
   private initialised: boolean = false;
+
   constructor(private route: ActivatedRoute, private cameraSvc: CameraService, private domSanitizer: DomSanitizer) {
     this.route.paramMap.subscribe((paramMap) => {
       let camera: string = paramMap.get('camera') as string;
       camera = atob(camera);
-      let cams = cameraSvc.getCameras()
+      let cams = cameraSvc.getCameras();
       cams.forEach((cam) => {
         if (cam.address == camera) {
           this.cam = cam;
         }
       });
-      if(this.initialised)
+      if (this.initialised) {
         this.ngOnInit();
+      }
     });
   }
 
   ngOnInit(): void {
-    if(this.cam !== undefined)
+    if (this.cam !== undefined) {
       this.cameraSvc.getAccessToken(this.cam.address, this.defaultPort).subscribe((result) => {
-        this.accessToken = result.accessToken;
-        this.hostServiceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(window.location.protocol+'//'+window.location.hostname+':'+environment.camAdminHostPort+'/?accessToken='+this.accessToken)
-        this.intervalSubscription = interval(10000).subscribe(() => {
-          this.cameraSvc.resetTimer(this.accessToken).subscribe(() => {},
-            error => {
+          this.accessToken = result.accessToken;
+          this.hostServiceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(window.location.protocol + '//' + window.location.hostname + ':' + environment.camAdminHostPort + '/?accessToken=' + this.accessToken);
+          this.intervalSubscription?.unsubscribe();
+          this.intervalSubscription = interval(10000).subscribe(() => {
+            this.cameraSvc.resetTimer(this.accessToken).subscribe(() => {
+              },
+              error => {
                 this.reporting.errorMessage = error;
-                this.intervalSubscription.unsubscribe();
-            });
-        })
-      },
-      reason => {
-        this.reporting.errorMessage = reason;
-      })
+                this.intervalSubscription?.unsubscribe();
+              });
+          });
+        },
+        reason => {
+          this.reporting.errorMessage = reason;
+        });
+    }
     this.initialised = true;
   }
 
@@ -58,7 +63,6 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
   }
 
   ngOnDestroy(): void {
-    if(this.intervalSubscription !== undefined)
-      this.intervalSubscription.unsubscribe();
+    this.intervalSubscription?.unsubscribe();
   }
 }
