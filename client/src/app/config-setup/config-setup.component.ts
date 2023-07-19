16ade0f24e2224c79a8ec75ae687f58ea24367f1
richard-austin <richard.david.austin@gm>
2023-07-19 14:53:23 +0100

    Fixed: Cannot enter Camera IP Address when Camera type selection is "Not Listed"

--- client/src/app/config-setup/config-setup.component.ts
+++ client/src/app/config-setup/config-setup.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, ElementRef, isDevMode, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {CameraService, cameraType} from '../cameras/camera.service';
+import {CameraService} from '../cameras/camera.service';
 import {Camera, CameraParamSpec, Stream} from "../cameras/Camera";
 import {ReportingComponent} from '../reporting/reporting.component';
 import {animate, state, style, transition, trigger} from '@angular/animations';
