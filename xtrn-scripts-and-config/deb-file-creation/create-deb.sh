2c86eabf05e1240b2712c7e936de160bcfc0950c
richard-austin <richard.david.austin@gm>
2023-07-28 17:10:39 +0100

    Initial build settings for Ubuntu 23.04 (Get the OS install right this time)

--- xtrn-scripts-and-config/deb-file-creation/create-deb.sh
+++ xtrn-scripts-and-config/deb-file-creation/create-deb.sh
@@ -3,71 +3,71 @@
 export VERSION
 VERSION=$(< ../../server/grails-app/assets/version/version.txt)
 
-rm -r security-cam_*_armhf
+rm -r security-cam_*_arm64
 
-mkdir -p security-cam_"${VERSION}"_armhf/etc/security-cam
+mkdir -p security-cam_"${VERSION}"_arm64/etc/security-cam
 
 cp ../start_hd_recording.sh ../end_hd_recording.sh ../processmotionrecordings.sh \
   ../cacert.jks ../client.jks ../productKeyGen/generateProductKey.jar ../productKeyGen/publicKey \
   ../productKeyGen/productKeyGen.sh \
- security-cam_"${VERSION}"_armhf/etc/security-cam
+ security-cam_"${VERSION}"_arm64/etc/security-cam
 
-mkdir -p security-cam_"${VERSION}"_armhf/etc/security-cam/wifi-setup-service &&
-cp ../../wifi-setup-service/src/*.py security-cam_"${VERSION}"_armhf/etc/security-cam/wifi-setup-service
-cp ../../wifi-setup-service/src/requirements.txt security-cam_"${VERSION}"_armhf/etc/security-cam/wifi-setup-service
+mkdir -p security-cam_"${VERSION}"_arm64/etc/security-cam/wifi-setup-service &&
+cp ../../wifi-setup-service/src/*.py security-cam_"${VERSION}"_arm64/etc/security-cam/wifi-setup-service
+cp ../../wifi-setup-service/src/requirements.txt security-cam_"${VERSION}"_arm64/etc/security-cam/wifi-setup-service
 
-mkdir -p security-cam_"${VERSION}"_armhf/etc/fmp4-ws-media-server &&
-cp  ../fmp4-ws-media-server/fmp4-ws-media-server_arm64 security-cam_"${VERSION}"_armhf/etc/fmp4-ws-media-server
-cp  ../fmp4-ws-media-server/config.json security-cam_"${VERSION}"_armhf/etc/fmp4-ws-media-server
+mkdir -p security-cam_"${VERSION}"_arm64/etc/fmp4-ws-media-server &&
+cp  ../fmp4-ws-media-server/fmp4-ws-media-server_arm64 security-cam_"${VERSION}"_arm64/etc/fmp4-ws-media-server
+cp  ../fmp4-ws-media-server/config.json security-cam_"${VERSION}"_arm64/etc/fmp4-ws-media-server
 
-mkdir -p security-cam_"${VERSION}"_armhf/etc/security-cam/camera-recordings-service &&
-cp  ../../camera-recordings-service/src/*.py security-cam_"${VERSION}"_armhf/etc/security-cam/camera-recordings-service
-cp  ../../camera-recordings-service/src/requirements.txt security-cam_"${VERSION}"_armhf/etc/security-cam/camera-recordings-service
+mkdir -p security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service &&
+cp  ../../camera-recordings-service/src/*.py security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
+cp  ../../camera-recordings-service/src/requirements.txt security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
 
-mkdir -p security-cam_"${VERSION}"_armhf/lib/systemd/system
-cp ../wifimanagement.service  security-cam_"${VERSION}"_armhf/lib/systemd/system
-cp ../camera-recordings.service security-cam_"${VERSION}"_armhf/lib/systemd/system
-cp ../fmp4-ws-media-server.service security-cam_"${VERSION}"_armhf/lib/systemd/system
+mkdir -p security-cam_"${VERSION}"_arm64/lib/systemd/system
+cp ../wifimanagement.service  security-cam_"${VERSION}"_arm64/lib/systemd/system
+cp ../camera-recordings.service security-cam_"${VERSION}"_arm64/lib/systemd/system
+cp ../fmp4-ws-media-server.service security-cam_"${VERSION}"_arm64/lib/systemd/system
 
-mkdir -p security-cam_"${VERSION}"_armhf/DEBIAN
-cp preinst postinst prerm postrm security-cam_"${VERSION}"_armhf/DEBIAN
+mkdir -p security-cam_"${VERSION}"_arm64/DEBIAN
+cp preinst postinst prerm postrm security-cam_"${VERSION}"_arm64/DEBIAN
 
-mkdir -p security-cam_"${VERSION}"_armhf/var/security-cam/recording-pids
-mkdir -p security-cam_"${VERSION}"_armhf/var/security-cam/motion/conf.d
+mkdir -p security-cam_"${VERSION}"_arm64/var/security-cam/recording-pids
+mkdir -p security-cam_"${VERSION}"_arm64/var/security-cam/motion/conf.d
 
-mkdir -p security-cam_"${VERSION}"_armhf/var/security-cam/ftp
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec1
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec2
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec3
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec4
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec5
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec6
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec7
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec8
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec9
-mkdir security-cam_"${VERSION}"_armhf/var/security-cam/rec10
-mkdir -p security-cam_"${VERSION}"_armhf/var/log/security-cam
-mkdir -p security-cam_"${VERSION}"_armhf/var/log/camera-recordings-service
-mkdir -p security-cam_"${VERSION}"_armhf/var/log/fmp4-ws-media-service
+mkdir -p security-cam_"${VERSION}"_arm64/var/security-cam/ftp
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec1
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec2
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec3
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec4
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec5
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec6
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec7
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec8
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec9
+mkdir security-cam_"${VERSION}"_arm64/var/security-cam/rec10
+mkdir -p security-cam_"${VERSION}"_arm64/var/log/security-cam
+mkdir -p security-cam_"${VERSION}"_arm64/var/log/camera-recordings-service
+mkdir -p security-cam_"${VERSION}"_arm64/var/log/fmp4-ws-media-service
 
-mkdir -p security-cam_"${VERSION}"_armhf/var/log/motion
-mkdir -p security-cam_"${VERSION}"_armhf/var/log/wifimgr
-mkdir -p security-cam_"${VERSION}"_armhf/var/lib/tomcat
+mkdir -p security-cam_"${VERSION}"_arm64/var/log/motion
+mkdir -p security-cam_"${VERSION}"_arm64/var/log/wifimgr
+mkdir -p security-cam_"${VERSION}"_arm64/var/lib/tomcat
 
-mkdir -p security-cam_"${VERSION}"_armhf/tmp
+mkdir -p security-cam_"${VERSION}"_arm64/tmp
 
-mkdir -p security-cam_"${VERSION}"_armhf/lib/systemd/system/
+mkdir -p security-cam_"${VERSION}"_arm64/lib/systemd/system/
 
-cp -r ../motion/motion.conf ../nginx.conf ../chrony.conf ../ssmtp.conf security-cam_"${VERSION}"_armhf/tmp
-cp ../apache-tomcat-9.0.46/conf/server.xml ../apache-tomcat-9.0.46/conf/tomcat-users.xml security-cam_"${VERSION}"_armhf/tmp
-cp ../install-cert.sh ../tomcat9 security-cam_"${VERSION}"_armhf/tmp
-cp ../../server/build/libs/server-7.3.war security-cam_"${VERSION}"_armhf/tmp
-cp ../../initialAdmin/dist/cua.war  security-cam_"${VERSION}"_armhf/tmp
+cp -r ../motion/motion.conf ../nginx.conf ../chrony.conf ../ssmtp.conf security-cam_"${VERSION}"_arm64/tmp
+cp ../apache-tomcat-9.0.46/conf/server.xml ../apache-tomcat-9.0.46/conf/tomcat-users.xml security-cam_"${VERSION}"_arm64/tmp
+cp ../install-cert.sh ../tomcat9 security-cam_"${VERSION}"_arm64/tmp
+cp ../../server/build/libs/server-7.3.war security-cam_"${VERSION}"_arm64/tmp
+cp ../../initialAdmin/dist/cua.war  security-cam_"${VERSION}"_arm64/tmp
 
-cat << EOF > security-cam_"${VERSION}"_armhf/DEBIAN/control
+cat << EOF > security-cam_"${VERSION}"_arm64/DEBIAN/control
 Package: security-cam
 Version: $VERSION
-Architecture: armhf
+Architecture: arm64
 Maintainer: Richard Austin <richard.david.austin@gmail.com>
 Description: A security camera system accessed through a secure web based interface.
 Depends: openjdk-18-jre-headless (>=18.0.2), openjdk-18-jre-headless (<< 18.9.9),
@@ -84,4 +84,4 @@ Depends: openjdk-18-jre-headless (>=18.0.2), openjdk-18-jre-headless (<< 18.9.9)
  python3-pip
 EOF
 
-dpkg-deb --build --root-owner-group security-cam_"${VERSION}"_armhf
+dpkg-deb --build --root-owner-group security-cam_"${VERSION}"_arm64
