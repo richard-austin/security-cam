212176db01ec6b0f977f1e0166feaa7e90df9632
richard-austin <richard.david.austin@gm>
2024-08-16 16:50:23 +0100

    * Include Python modules in gradle. These can be bootRun, but the wifi-setup-service should run as root to function, so it can't be properly run in development this way.

--- camera-recordings-service/untitled/go.mod
+++
@@ -1,3 +0,0 @@
-module untitled
-
-go 1.19
--- xtrn-scripts-and-config/deb-file-creation/create-deb.sh
+++ xtrn-scripts-and-config/deb-file-creation/create-deb.sh
@@ -22,8 +22,8 @@ cp  ../fmp4-ws-media-server/fmp4-ws-media-server_arm64 security-cam_"${VERSION}"
 cp  ../fmp4-ws-media-server/config.json security-cam_"${VERSION}"_arm64/etc/fmp4-ws-media-server
 
 mkdir -p security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service &&
-cp  ../../camera-recordings-service/src/*.py security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
-cp  ../../camera-recordings-service/src/requirements.txt security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
+cp  ../../camera-recordings-service/*.py security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
+cp  ../../camera-recordings-service/requirements.txt security-cam_"${VERSION}"_arm64/etc/security-cam/camera-recordings-service
 
 mkdir -p security-cam_"${VERSION}"_arm64/lib/systemd/system
 cp ../wifimanagement.service  security-cam_"${VERSION}"_arm64/lib/systemd/system
