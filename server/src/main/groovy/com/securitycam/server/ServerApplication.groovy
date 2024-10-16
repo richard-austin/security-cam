5387e193479ced6a83f703285b871b3bf6162266
richard-austin <richard.david.austin@gm>
2024-10-16 21:24:21 +0100

    * New spring boot server project

---
+++ server/src/main/groovy/com/securitycam/server/ServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.securitycam.server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ServerApplication, args)
+    }
+
+}
