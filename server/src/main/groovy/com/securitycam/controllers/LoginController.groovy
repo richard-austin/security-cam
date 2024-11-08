f943eb62c404ab0530ff0731454b92683ecd70fe
richard-austin <richard.david.austin@gm>
2024-11-08 16:48:53 +0000

    * Fix bugs with auth settings for password reset MVC components * Redirect from /login to / when logged in.

---
+++ server/src/main/groovy/com/securitycam/controllers/LoginController.groovy
@@ -0,0 +1,19 @@
+package com.securitycam.controllers
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class LoginController {
+    @GetMapping("/login")
+    def login() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication()
+        if (auth && auth.authorities[0] == new SimpleGrantedAuthority('ROLE_ANONYMOUS'))
+            return  'login'
+        else
+            return 'redirect:/'
+    }
+}
