dde82e8165eec9542ca1e54abc56ad855df23bf0
richard-austin <richard.david.austin@gm>
2024-12-06 20:29:10 +0000

    * buildDebFile (not now bootWar) depends on setVersion

--- server/src/main/groovy/com/securitycam/security/MyUserDetailsService.java
+++ server/src/main/groovy/com/securitycam/security/MyUserDetailsService.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service("userDetailsService")
-//@Transactional
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
