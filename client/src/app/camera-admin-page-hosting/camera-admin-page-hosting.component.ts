ad1c002d86752832ad236f7c9ebafb50132f44cb
richard-austin <richard.david.austin@gm>
2025-10-16 14:21:24 +0100

    * Close admin page tab on exiting to admin page component or switching to another device.

--- client/src/app/camera-admin-page-hosting/camera-admin-page-hosting.component.ts
+++ client/src/app/camera-admin-page-hosting/camera-admin-page-hosting.component.ts
@@ -3,7 +3,7 @@ import {CameraService} from '../cameras/camera.service';
 import {ActivatedRoute} from '@angular/router';
 import {ReportingComponent} from '../reporting/reporting.component';
 import {interval, Subscription} from 'rxjs';
-import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {SafeResourceUrl} from '@angular/platform-browser';
 import {environment} from '../../environments/environment';
 import {UtilsService} from "../shared/utils.service";
 
@@ -13,7 +13,7 @@ import {UtilsService} from "../shared/utils.service";
   templateUrl: './camera-admin-page-hosting.component.html',
   imports: [
     ReportingComponent
-],
+  ],
   styleUrls: ['./camera-admin-page-hosting.component.scss']
 })
 export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, OnDestroy {
@@ -23,6 +23,7 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
   @ViewChild(ReportingComponent) reporting!: ReportingComponent;
   private webAdminPort: number = 80;
   private initialised: boolean = false;
+  private tabHandle: Window | null = null;
 
   constructor(private route: ActivatedRoute, private cameraSvc: CameraService, utils: UtilsService) {
     this.route.paramMap.subscribe((paramMap) => {
@@ -37,10 +38,10 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
           this.webAdminPort = 80;   // TODO: Probably need to set the port in the camera info
         }
       });
-      if(this.address === undefined || this.address !== address) {
+      if (this.address === undefined || this.address !== address) {
         let devices = utils.adHocDevices;
         devices.forEach((device) => {
-          if(device.ipAddress === address) {
+          if (device.ipAddress === address) {
             this.address = address;
             this.webAdminPort = device.ipPort;
           }
@@ -51,11 +52,12 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
       }
     });
   }
+
   makeId(length: number) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
@@ -65,8 +67,11 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
   ngOnInit(): void {
     if (this.address !== undefined) {
       this.cameraSvc.getHostingAccess(this.address, this.webAdminPort).subscribe(() => {
-         // this.hostServiceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('http://' + window.location.hostname + ':' + environment.camAdminHostPort + '/?randomId=' + this.makeId(12));
-          window.open('http://' + window.location.hostname + ':' + environment.camAdminHostPort + '/?randomId=' + this.makeId(12), '_blank')?.focus();
+          // this.hostServiceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('http://' + window.location.hostname + ':' + environment.camAdminHostPort + '/?randomId=' + this.makeId(12));
+          if (this.tabHandle)
+            this.tabHandle.close();
+
+          this.tabHandle = window.open('http://' + window.location.hostname + ':' + environment.camAdminHostPort + '/?randomId=' + this.makeId(12), '_blank');
           this.intervalSubscription?.unsubscribe();
           this.intervalSubscription = interval(10000).subscribe(() => {
             this.cameraSvc.resetTimer().subscribe(() => {
@@ -90,5 +95,7 @@ export class CameraAdminPageHostingComponent implements OnInit, AfterViewInit, O
   ngOnDestroy(): void {
     this.cameraSvc.closeClient().subscribe();
     this.intervalSubscription?.unsubscribe();
+    if (this.tabHandle)
+      this.tabHandle.close();
   }
 }
